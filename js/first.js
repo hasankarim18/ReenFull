9c18c710710c66e5743129d123af8437be838507
hasankarim18 <hallysust@gmail.com>
2020-09-22 16:47:32 +0600

    reenFull

---
+++ js/first.js
@@ -0,0 +1,34 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+	autoplay:true,
+	autoplayTimeout:4000,
+	
+    responsiveClass:true,
+	
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:true
+        }
+    }
+	
+})
+
+
+$(document).ready(function(){
+  $("#sreach_icon").click(function(){
+    $(this).hide();
+	 $("#search_box").show();
+  });
+  
+});
